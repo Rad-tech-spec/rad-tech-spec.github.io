e18ae07579a41376d440968682f1fb7453a7512f
Rad Eshghi <reshghi@myseneca.ca>
2025-09-24 16:34:02 -0400

    Updated packages

--- .output/public/_nuxt/CDjlar5h.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Bj9mGHVW.js";import"./BgkWHpPr.js";import"./C-v3KzvZ.js";import"./CxYJgpPD.js";import"./uNDdkxDK.js";export{o as default};
--- .output/public/_nuxt/Cdx7yMgr.js
+++
@@ -1,1 +0,0 @@
-import{l as n}from"./BgkWHpPr.js";import{u as s}from"./B8SBFC7O.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const l=n({...t._flatMeta});return delete t._flatMeta,{...t,meta:l}}})}export{i as u};
